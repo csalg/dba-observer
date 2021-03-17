e0939703507c7b3efbc6d870126b66de7fa101f3
Carlos Salgado <charlie@csalgado.io>
2021-03-18 00:22:02 +0100

    Implemented sequelize DAO

--- src/config.ts
+++ src/config.ts
@@ -2,5 +2,5 @@ export default {
     PATH: "./db.sqlite",
     DBA_PAGE_URL: 'https://www.dba.dk/boliger/lejebolig/lejelejlighed/antalvaerelser-2/?pris=(4000-8000)&soegfra=2860&radius=7',
     MAX_PAGES_TO_PARSE: 100,
-    SECONDS_BETWEEN_SCRAPES: 60
+    SECONDS_BETWEEN_SCRAPES: 600
 }
