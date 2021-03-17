d46673fb97130a9c3222c057c7193ed85b2c845c
Carlos Salgado <charlie@csalgado.io>
2021-03-18 00:43:37 +0100

    Cast for numeric environment variables

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 export default {
     PATH: process.env['PATH'] || "./db.sqlite",
     DBA_PAGE_URL: process.env['DBA_PAGE_URL'] || 'https://www.dba.dk/boliger/lejebolig/lejelejlighed/antalvaerelser-2/?pris=(4000-8000)&soegfra=2860&radius=7',
-    MAX_PAGES_TO_PARSE: process.env['MAX_PAGES_TO_PARSE'] || 100,
-    SECONDS_BETWEEN_SCRAPES: process.env['SECONDS_BETWEEN_SCRAPES'] || 600
+    MAX_PAGES_TO_PARSE: parseInt(process.env['MAX_PAGES_TO_PARSE']) || 100,
+    SECONDS_BETWEEN_SCRAPES: parseInt(process.env['SECONDS_BETWEEN_SCRAPES']) || 600
 }
